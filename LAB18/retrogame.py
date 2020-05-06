7775031697cba721fb39e9f6e9711e5a2d61e798
mottrelleg <woodwerker@gmail.com>
2020-05-06 13:03:48 -0400

    Looping techniques

---
+++ LAB18/retrogame.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+message="Based on your budget, I recommend that you purchase "
+budget=int(input("How much money do you have to spend on your Retrogaming solution?(in dollars): "))
+
+
